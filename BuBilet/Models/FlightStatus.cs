dc52212e25e866b329cc270476678957e32ef509
Hasan Çakır <memseha@gmail.com>
2023-12-23 15:45:39 +0300

    day 4 ?

---
+++ BuBilet/Models/FlightStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuBilet.Models
+{
+    public class FlightStatus
+    {
+        [Key]
+        public int StatusID { get; set; }
+        [StringLength(100)]
+        public string StatusDescription { get; set; }
+
+        // Navigation property
+        public virtual ICollection<FlightStatusUpdates> FlightStatusUpdates { get; set; }
+
+    }
+}
