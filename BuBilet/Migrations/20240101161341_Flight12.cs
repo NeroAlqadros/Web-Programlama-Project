2ba961788021a108fd8d946edb60f70aaa0d3019
Hasan Çakır <memseha@gmail.com>
2024-01-01 19:39:39 +0300

    day 13 ?

---
+++ BuBilet/Migrations/20240101161341_Flight12.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuBilet.Migrations
+{
+    public partial class Flight12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SeatId",
+                table: "Ticket",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeatId",
+                table: "Ticket");
+        }
+    }
+}
